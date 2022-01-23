49d4faa5ec6275751778449141ea147d0c0d3499
Razsaka <razsaka01@yahoo.com>
2022-01-23 09:54:17 -0500

    updated

--- client-side/js/components/Home.js
+++ client-side/js/components/Home.js
@@ -1,9 +1,31 @@
 
 
-export default function Home() {
+import Items from './Items';
+import PopularItems from './PopularItems';
+import apiHelpers from '../api-helpers/apiHelpers';
+
+const [popularItems, setPopularItems] = apiHelpers.useState();
+
+apiHelpers.getRequest('http://localhost:8080/api/popular-items', (popularItems) => {
+  setPopularItems(popularItems);
+});
+
+
+export default function Home(items) {
   return `
   <div class='page__container'>
-
+    <div class='form__container'>
+        <input  class='add-item__name' type='text' placeholder='Enter item'>
+        <button class='add-item__submit'>Submit</button>
+        <section class='itemList'>
+         ${Items(items)}
+        </section>
+      </div>
+      <div class='warning__container'>
+        <h2 class='warning'></h2>
+      </div>
+      ${PopularItems(popularItems())}
+    </div> 
    </div>
     `;
 }
